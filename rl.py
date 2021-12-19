797cdd30d3d90f3082efb78175250618080a9391
Martin Palovic <80816673+mtpalovic@users.noreply.github.com>
2021-12-19 20:26:01 +0100

    Add files via upload

---
+++ rl.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# reinforcement learning
+
+# In[1]:
+
+
+import numpy as np
+from numpy.random import randint
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[ ]:
+
+
+
+

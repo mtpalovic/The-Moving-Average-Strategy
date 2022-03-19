a2396cb3706e3c6ae3176ef76be4f4d80d56ead7
Martin Palovic <80816673+mtpalovic@users.noreply.github.com>
2022-03-19 12:50:30 +0100

    Add files via upload

--- p_2.py
+++ p_2.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[52]:
+# In[3]:
 
 
 import numpy as np
 from scipy import linspace, polyval, polyfit, sqrt, stats, randn, optimize
-
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,56 +14,95 @@ import csv
 
 import scipy
 from sklearn.linear_model import LinearRegression
+
+import matplotlib as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# In[25]:
-
-
-with open(r"C:\Users\mpalovic\Desktop\d.csv") as f:
-    r = csv.reader(f)
-    
-    table = []
-    
-    
-    for row in r:
-        print(row)
-print(r)
-
-
-# In[41]:
-
-
-data= pd.read_csv(r"C:\Users\mpalovic\Desktop\d.csv")
-data.iloc[]
-
-
-# In[47]:
-
-
-lp = {}
-for i,v in enumerate(data.columns.values):
-    lp.append(i,v)
-
-
-# In[48]:
-
-
-lp
+import sphinx
 
 
 # In[ ]:
 
 
-class Brownian():
+class lr(object):
     """
     Class constructor.
     """
-    def __init__(self,x0):
+    def __init__(self,x0,y0,l,iter_):
         """
-        Init class.
+        Constructor method.
         """
-        assert (type(x0)==float or type(x0)==int or type(x0)==None)
         
-        self.x0 = float(x0)
+        self.x0 = x0
+        self.y0 = y0
+        
+        self.l = float(l)
+        #forces to be float or init, otherwise error raised
+        assert (type(l)==float or type(l)==int or type(x0)==None)
+        
+        
+        self.iter_ = iter_
+        self.n = len(self.x0)
+        
+        #weight,bias, not part of init
+        self.a = 0
+        self.b = 0
+        
+        self.h = np.zeros(self.iter_)
+        
+    def c(self,y_):
+        """
+        Cost function
+        :param y_: init method
+        :type y_: init method
+        
+        :return:
+        :rtype: 
+        
+        """
+        c = np.sum(np.square(self.y0 - y_))/(2*self.n)
+        return c 
+    
+    def f(self):
+        """
+        Fit function
+        :param y_: init method
+        :type y_: init method
+        
+        :return:
+        :rtype: 
+        
+        """
+        for e in range(0,len(self.iters_),1):
+            
+            y_ = self.b*self.X + self.a
+            
+            dv_a = (-2/self.n)*(self.y0 - y_)
+            
+            dv_b = (-2/self.n)*(self.x0*(self.y0 - y_))
+            
+            
+            self.a = self.a - dv_a*self.l
+            self.b = self.b - dv_b*self.l
+            
+            self.history[e] = self.c(y_)
+            self.mse = self.mean_se(y_,self.y0)
+        
+        return self.mse
+    
+    
+    def mean_se(self,y_p,y)
+        """
+        Fit function
+        :param y_: init method
+        :type y_: init method
+        
+        :return:
+        :rtype: 
+        
+        """
+        er = y - y_p
+        mse = np.sum(np.square(er))/self.n
+        
+        return mse
 

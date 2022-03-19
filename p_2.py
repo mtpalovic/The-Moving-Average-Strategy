1114ba7dc0683544dc911da60879377392bca029
Martin Palovic <80816673+mtpalovic@users.noreply.github.com>
2022-03-19 13:14:58 +0100

    Add files via upload

--- p_2.py
+++ p_2.py
@@ -21,14 +21,16 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 import sphinx
 
 
-# In[ ]:
+# In[3]:
 
 
 class lr(object):
     """
     Class constructor.
     """
-    def __init__(self,x0,y0,l,iter_):
+    N_ITERS: 1000
+    
+    def __init__(self,x0,y0,l):
         """
         Constructor method.
         """
@@ -41,15 +43,31 @@ class lr(object):
         assert (type(l)==float or type(l)==int or type(x0)==None)
         
         
-        self.iter_ = iter_
+        
         self.n = len(self.x0)
         
         #weight,bias, not part of init
         self.a = 0
         self.b = 0
         
-        self.h = np.zeros(self.iter_)
-        
+        self.h = np.zeros(lr.N_ITERS)
+    
+    
+    
+    
+    
+    def __str__(self):
+        """String representation of an object. Init params are instance attributes.
+        :return: init param as string
+        :rtype: str
+        """
+        return f"init params:{self.x0},{self.y0}, {self.l}"
+    
+    
+    
+    
+    
+    
     def c(self,y_):
         """
         Cost function
@@ -86,12 +104,12 @@ class lr(object):
             self.b = self.b - dv_b*self.l
             
             self.history[e] = self.c(y_)
-            self.mse = self.mean_se(y_,self.y0)
+            self.mse = self.mean_se(self.y0, y_)
         
         return self.mse
     
     
-    def mean_se(self,y_p,y)
+    def mean_se(self,y_p,y):
         """
         Mean squared error.
         :param y_p: 
@@ -108,4 +126,42 @@ class lr(object):
         mse = np.sum(np.square(er))/self.n
         
         return mse
+    
+    
+    
+    def create_list(self,*args):
+        self.li = [*args]
+        return self.li
+    
+    
+    def create_tuple(self):
+        """Creates a tuple
+        :return:
+        :rtype: tuple
+        """
+        self.tupl = ()
+        
+        return self.tupl
+    
+    
+     def create_dict(self):
+        """Creates a dictionary
+        :return:
+        :rtype:
+        """
+        self.dict = {}
+        
+        return self.dict
+
+
+# In[4]:
+
+
+q = lr(100,10,800)
+
+
+# In[ ]:
+
+
+
 

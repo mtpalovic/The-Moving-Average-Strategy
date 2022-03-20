9484ada8180d5aff7e6dd9b98da0a2d6345a70c7
Martin Palovic <80816673+mtpalovic@users.noreply.github.com>
2022-03-20 11:49:39 +0100

    Add files via upload

--- pr1.py
+++ pr1.py
@@ -5,43 +5,38 @@
 
 
 import numpy as np
-
 import matplotlib.pyplot as plt
+import random
 
 
 # In[ ]:
 
 
-class lr(object):
+class linreg(object):
     """
     Class constructor.
     """
-    N_ITERS = 100
     
-    def __init__(self,x0:list = None,y0:list = None,l:float = 0.7):
+    def __init__(self,x0:list,y0:list,l:float = 0.005,n_iters:int=10000):
         """
         Constructor method.
         """
-        
         self.x0 = x0
-        assert type(x0)==list
-        
-        
         self.y0 = y0
-        assert type(y0)==list
-        
+        assert (type(x0)==list and type(y0)==list)
         
         self.l = l
-        #forces to be float or init, otherwise error raised
         assert (type(l)==float or type(l)==int)  
         
         
-        self.n = len(self.x0)
-        self.h = np.zeros(lr.N_ITERS)
+        self.n_iters = n_iters
+        self.n = len(self.x0) 
+        self.h = np.zeros(self.n_iters)
         
         #weight,bias, not part of init
-        self.a = None
-        self.b = None
+        self.a = 2
+        self.b = 3
+        self.mse = 0
     
     
     
@@ -54,92 +49,70 @@ class lr(object):
     
     
     
-    def c(self,y_):
+    def _c(self,y_:float):
         """
-        Cost function
+        Cost function. Predicts cost of one estim value and all data points.
         :param y_: estimated value of y
-        :type y_: int or float, required
+        :type y_: float, required
         
         :return: cost associated with estimated value of y
-        :rtype: int or float
+        :rtype: float
         
         """
         
-        for i,v in enumerate(self.y0):
-            
-            c = np.sum(np.square(self.y0[i] - y_))/(2*self.n)
-            c = float(c)
-        
-        return c 
+        if y_:
+            if isinstance(y_,float):
+                c = 0
+                for i,v in enumerate(self.y0):
+                    c = np.sum(np.square(self.y0[i] - y_))/(2*self.n)
+                    c = float(c)
+        return c
     
     
     
-    def f(self):
+    def _f(self):
         """
         Fit function
-        :param y_: 
-        :type y_: 
-        
-        :return:
-        :rtype: 
+        :return: mean squared error
+        :rtype: float
         
         """
-        self.h = np.zeros(lr.N_ITERS)
         
-        for e in range(lr.N_ITERS):
-            
+        #creates a list of length of n_iters
+        self.h = np.zeros(self.n_iters)
+        
+        for e in range(len(self.h)):
             for i,(v,k) in enumerate(zip(self.x0,self.y0)):
-                
                 y_ = self.b*self.x0[i] + self.a
-            
                 dv_a = (-2/self.n)*(self.y0[i] - y_)
-            
                 dv_b = (-2/self.n)*(self.x0[i]*(self.y0[i] - y_))
-            
+                self.a = self.a - dv_a*self.l
+                self.b = self.b -dv_b*self.l
                 
-                
-                self.a -= dv_a*self.l
-                
-                self.b -= dv_b*self.l
-             
-                
-                self.mse = self.mean_se(y_,self.y0)
-            
-            
-            self.h[e] = self.c(y_)
- 
-        
+                self.mse = self._mse(y_,self.y0)
+            self.h[e] = self._c(y_)
         return self.mse
     
     
     
     
     
-    def mean_se(self,y_p,y0):
+    def _mse(self,y_p:float,y0:list):
         """
         Mean squared error.
-        :param y_p: 
-        :type y_p: 
+        :param y_p: estimated value of y from _f()
+        :type y_p: float
         
-        :param y: 
-        :type y: 
+        :param y0: y0 observations
+        :type y0: list 
         
-        :return:
-        :rtype:
+        :return: mean squared error
+        :rtype: float
         
         """
         for i,v in enumerate(y0):
-            
             mse = (1/self.n)*np.sum(np.square(y0[i] - y_p))
-        
         return mse
-    
-    
-    
-    
-    def create_list(self,*args):
-        self.li = [*args]
-        return self.li
     
     
     def create_tuple(self):
@@ -147,9 +120,9 @@ class lr(object):
         :return:
         :rtype: tuple
         """
-        self.tupl = ()
+        self.tuple = ()
         
-        return self.tupl
+        return self.tuple
     
     
     def create_dict(self):
@@ -162,13 +135,10 @@ class lr(object):
         return self.dict
     
     
-    
-    def res(self):
+    def _res(self):
         
         f=plt.figure(figsize=(10,10))
-        q=f.add_subplot(211)
-
+        z=f.add_subplot(211)
         plt.title("error minimisation for a number of iterations")
-        
-        q.plot(self.h)
+        z.plot(self.h)
 

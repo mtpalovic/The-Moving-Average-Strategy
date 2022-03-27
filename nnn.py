52eb4897f286e32abeda961f9492cb04fa8832fa
Martin Palovic <80816673+mtpalovic@users.noreply.github.com>
2022-03-27 15:45:53 +0200

    Add files via upload

--- nnn.py
+++ nnn.py
@@ -1,21 +1,49 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 import numpy as np
+import random
+random.seed(43)
+
+
 import matplotlib.pyplot as plt
 
 
-# In[4]:
+# In[ ]:
+
+
+np.random.seed(43)
+a = np.random.randn(1000,4)
+a
+
+
+# In[ ]:
+
+
+np.random.seed(43)
+b = np.random.randn(1000,4)
+b
+
+
+# In[ ]:
+
+
+np.random.seed(43)
+c = np.random.randn(1000,1)
+c
+
+
+# In[ ]:
 
 
 class nn(object):
     """
     Class constructor.
     """
-    def __init__(self,x0;np.ndarray,y0:np.ndarray,lr:float=0.001,n_iters:int=1000)
+    def __init__(self,x0:np.ndarray,y0:np.ndarray,lr:float=0.001,n_iters:int=1000):
         """
         Constructor method.
         """
@@ -26,34 +54,86 @@ class nn(object):
         self.m, self.n = np.shape(self.x0)[0], np.shape(self.x0)[1]
     
         
-        self.w = None
-        self.b = None
-    
-    def initialise(self,features):
-        
-        #randn takes shape of the output array from normal dist
-        w = np.random.randn(len(features),2)
-        b = 0
-        return w,b
+        self.w = np.random.randn(self.n,1)
+        self.b = 1
     
     
     def sigmoid(self,z):
         return 1/(1+np.exp(-z))
     
     
-    def forward_propagate(self,)
-        self.sigmoid(np.matmul(self.x0,w.T)+self.b)
+    def forward_propagate(self,vect:False):
+        if not vect:
+            #shape(1000,1)
+            A = self.sigmoid(np.matmul(self.x0,self.w)+self.b)
+            
+            cost = (-1/self.m)*np.sum(self.y0*np.log(A)+(1-self.y0)*(np.log(1-A)))
+            
+            d_w = np.dot(self.x0.T,(A-self.y0))
+            d_b = (1/self.m)*np.sum(A-self.y0)
+        
+            gradients = {
+                "d_w":d_w,
+                "d_b":d_b
+            }
+        
+        return cost
     
     
     
-    
+    def fwd_propagate(self):
+        h = np.zeros((1000,1))
+        for i in range(len(self.x0)):
+            for j in range(len(self.w[0])):
+                for k in range(len(self.w)):
+                    h[i][j] += self.x0[i][k]*self.w[k][j]
+        
+        B = self.sigmoid(h + self.b)
+        
+        
+        
+        c_m = 0
+        for z in range(self.m):
+            c_m += (self.y0[z]*np.log(B[z])+(1-self.y0[z])*(np.log(1-B[z])))
+        
+        c = (-1/self.m)*c_m
+        
+        
+        
+        
+        
+        
+        return float(c)
 
 
-# In[9]:
+# In[ ]:
 
 
-a = np.random.randn(10,2)
-a.shape[1]
+neural_nets = nn(a,c,0.001,1000)
+
+
+# In[ ]:
+
+
+np.shape(neural_nets.x0)[1]
+
+
+# In[ ]:
+
+
+neural_nets.forward_propagate(False)
+
+
+# In[ ]:
+
+
+neural_nets.fwd_propagate()
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:

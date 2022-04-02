306bb972872df56bed5696da63979d3ed0eea3cc
Martin Palovic <80816673+mtpalovic@users.noreply.github.com>
2022-04-02 16:53:00 +0200

    Add files via upload

--- lr.py
+++ lr.py
@@ -1,14 +1,45 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# $$h_{\theta}(x^{i}) = \theta_{0} x_{0}^i+\theta_{1} x_{1}^i+\theta_{2} x_{2}^i+\theta_{j} x_{j}^i \tag{1}$$
+
+# $$J(\theta) = \frac{1}{2m}\sum\limits_{i=1}^{m}(h_{\theta}(x^{i})-y^{i})^2 \tag{2}$$
+
+# $$J(\theta) = \frac{1}{2m}\sum\limits_{i=1}^{m}((\theta_{0} x_{0}^i+\theta_{1} x_{1}^i+\theta_{2} x_{2}^i+\theta_{j} x_{j}^i)-y^{i})^2 \tag{3}$$
+
+# $$\frac{\partial J(\theta)}{\partial \theta_{j}} = \frac{1}{m}\sum\limits_{i=1}^{m}(h_{\theta}(x^{i})-y^{i})x_{j}^i \tag{4}$$
+
+# $$\frac{\partial J(\theta)}{\partial \theta_{j}} = \frac{1}{m}\sum\limits_{i=1}^{m}((\theta_{0} x_{0}^i+\theta_{1} x_{1}^i+\theta_{2} x_{2}^i+\theta_{j} x_{j}^i)-y^{i})x_{j}^i \tag{5}$$
+
+# $$\theta_{j} = \theta_{j} - \alpha \frac{1}{m}\sum\limits_{i=1}^{m}((\theta_{0} x_{0}^i+\theta_{1} x_{1}^i+\theta_{2} x_{2}^i+\theta_{j} x_{j}^i)-y^{i})x_{j}^i \tag{6}$$
+
+# $$J(\theta) = \frac{1}{2m}(x \theta - y)^T(x \theta - y) \tag{7}$$
+
+# $$\frac{\partial J(\theta)}{\partial \theta} = \frac{1}{m}x^T(x \theta - y) \tag{8}$$
+
 # In[ ]:
 
 
 import numpy as np
 import random
-random.seed(43)
+random.seed(63)
 
 import matplotlib.pyplot as plt
+
+
+# In[ ]:
+
+
+#generate random data with seed for reproducibility
+np.random.seed(63)
+a = np.random.randn(1000,10)
+
+
+# In[ ]:
+
+
+np.random.seed(63)
+b = np.random.randn(1000,1)
 
 
 # In[ ]:
@@ -19,7 +50,7 @@ class lr(object):
     Class constructor.
     """
     
-    def __init__(self,x0:np.ndarray, y0:np.ndarray,lr:float=0.001,n_iters:int=10000):
+    def __init__(self,x0:np.ndarray, y0:np.ndarray,lr:float=0.001,n_iters:int=1000):
         """
         Constructor method.
         """
@@ -37,54 +68,35 @@ class lr(object):
         
         # 1D array of size n_features
         #initialise theta
-        self.th = np.ones(self.n)
+        #column vector of of shape(10,1)
+        self.th = np.random.randn(self.n,1)
         
-     
     
-    
-    
-    def create_array(self):
-        """Creates a list
-        :return:
-        :rtype: list
-        """
         
-        self.array = []
-        
-        return self.array
     
     
-    
-    
-    
-    def cost_not_vectorised(self):
-        """
-        Cost function. Estimated parameters and true values across all rows.
-        
-        :return: cost associated with estimated value of y
-        :rtype: float
-        
-        """
+    def cost_not_vectorised(self, theta):
         
         #this indicates that cost will be a float
-        #sum cost across all rows
+        #calculate cost at each row or sample and then sum
         c = 0
-        
         for i in range(self.m):    
             
-            #y_hat for each row
+            #y_hat prediction for each row with all estimated parameters
+            #at each row, iterate over all columns/parameters stored in theta
             y_hat = 0
-            
             for j in range(self.n):
-                y_hat += self.th[j]*self.x0[i][j]
+                y_hat += theta[j]*self.x0[i][j]
             
             #np.power because of neg distance and punishing large outliers
-            #cost for each obs i
-            c_i = np.power((y_hat - self.y0[i]),2)
+            #cost for row i, substract actual from prediction
+            c_i = (y_hat - self.y0[i])**2
             c += c_i
+            c = float(c)
         
+        
+        #out of for loop
         cost = (1/(2*self.m))*c
-        
         
         return cost
     
@@ -92,30 +104,22 @@ class lr(object):
     
     
     def gradient_descent_not_vectorised(self):
-        """
-        Gradient descent using a for loop. Non-vectorised implementation.
         
-        :return: cost associated with estimated value of y
-        :rtype: float
+        np.random.seed(63)
+        self.th = np.random.randn(self.n,1)
         
-        """
-        
-        #calc cost for each iteration as gradient descent continues
-        cost_l = []
+        cost_list = []
         
         
         for _ in range(self.n_iters):
             
-            #only one derivative for each column/feature
             
-            dev_ = []
+            derivatives_list = []
             for k in range(self.n):
                 
                 
-                
-                
                 #sum derivatives from all rows
-                dev_sum = 0
+                d_j = 0
                 for i in range(self.m):
                     
                     #estimate
@@ -123,71 +127,92 @@ class lr(object):
                     for j in range(self.n):
                         y_hat += self.th[j]*self.x0[i][j]
                     
-                    #derivative for each row 
+                    #derivative for each row
+                    #cannot be indexed by j because out of that for loop at this step
+                    #index by k because derivative for k parameter 
+                    
+                    #derivative of theta j at row i
                     d_i = (y_hat - self.y0[i])*self.x0[i][k]
-                    dev_sum += d_i 
+                    
+                    #sum all rows and store as d_j, derivative of theta j
+                    d_j += d_i 
                 
                 
+                #once iterative over all 10 theta parameters
+                #append derivative for each column (of each parameter) to a list
+                derivatives = (1/self.m)*d_j
                 
-                
-                
-                #append to list
-                #derivative for each column
-                dev = (1/self.m)*dev_sum 
-                dev_.append(dev)
+                #derivatives = float(derivatives)
+                derivatives_list.append(derivatives)
+            
             
             
             #update params stored as self.th acc to lr rate
-            self.th = self.th - self.lr*np.array(dev_)
+            #both self.th and np.array(derivatives_list) are column vectors of shape (10,1)
+            #indexed by z, hence result is a column vector of original shape
             
-            cos = self.cost_not_vectorised()
-            
-            cost_l.append(cos)
-            
-            plt.plot(cost_l)
-        
-        return len(cost_l)
-    
-    
-    
-    
-    
-    
-    
-    
-    def cost_vectorised(self):
-        
-        
-        
-        if np.shape(self.x0)[1] == np.shape(self.th)[0]:
-        
+            derivatives_list = np.array(derivatives_list).reshape(-1,1)
+            for z in range(len(self.th)):
+                self.th[z] = self.th[z] - self.lr*derivatives_list[z]
             
             
-            a = self.x0@self.th
-            a = a.reshape(-1,1)
-            self.th = np.reshape(self.th,(-1,1))
+            cost = self.cost_not_vectorised(self.th)
+            cost_list.append(cost)
+            
+            plt.plot(cost_list)
+        
+        return cost_list
+    
+    
+    
+    
+    
+    
+    
+    def cost_vectorised(self, theta):
         
         
-            if np.shape(a) == (np.size(self.x0,0),np.size(self.th,1)):
         
+        #self.x0 of shape (1000,10) and self.th of shape (10,1)
+        #the matrix multiplication result of shape (1000,1)
+        if np.shape(self.x0)[1] == np.shape(theta)[0]:
+        
+            a = self.x0@theta
+            a = a.reshape(-1,1) #shape (1000,1)
+            
+            theta = np.reshape(theta,(-1,1)) #to be sure that shape (10,1)
+            
+            
+            #vector shape (1000,1) necessary for column vector substraction in vectorised form
+            if np.shape(a) == (np.size(self.x0,0),np.size(theta,1)):
+        
+                #all three column vectors of shape (1000,1)
                 b = a - self.y0
                 
                 if np.shape(b.T)[1] == np.shape(b)[0]:
                 
-                    c = b.T@b
-                    cost = (1/(2*self.m))*c
+                    #b.T of shape (1,1000) and b of shape (1000,1)
+                    #matrix multiplication result of shape (1,1), which is a scalar
+                    #equation 7
+                    c = float(b.T@b)
                 
                 else:
+                    #if this not true, matrix multiplication cannot be calculated
                     print(f"{np.shape(b.T)[1]} must be equal to {np.shape(b)[0]}")
                 
                 
         
             else:
-                print(f"vector a must be of size ({np.size(self.x0,0)},{np.size(self.th,1)})")
+                #check if a = x theta is of correct shape
+                print(f"vector a must be of size ({np.size(self.x0,0)},{np.size(theta,1)})")
         
         
         else:
-            print(f"{np.shape(self.x0)[1]} not equal to {np.shape(self.th)[0]}")
+            #if this not true, matrix multiplication cannot be calculated
+            print(f"{np.shape(self.x0)[1]} not equal to {np.shape(theta)[0]}")
+        
+        
+        cost = (1/(2*self.m))*c
         
         
         return cost
@@ -201,54 +226,53 @@ class lr(object):
     def gradient_descent_vectorised(self):
         
         
+        np.random.seed(63)
+        self.th = np.random.randn(self.n,1)
+        
+        
+        #self.x0 shape (1000,10) and self.th (10,1)
+        #the product of matrix multiplication is of shape (1000,1)
+        #self.y0 shape (1000,1)
+        #e hence of shape (1000,1)
         e = np.reshape(self.x0@self.th,(-1,1)) - self.y0 
         
+        
         #reshape into column vector
+        #to ensure that shape (10,1)
         self.th = np.reshape(self.th,(-1,1))
         
         
-        c_k = []
+        cost_list1 = []
         
         for i in range(self.n_iters):
             
-            if np.shape(self.x0.T)[1] == np.shape(e)[0]:
+            #if np.shape(self.x0.T)[1] == np.shape(e)[0]:
+            
                 
-                self.th = self.th - self.lr*(1/self.m)*(self.x0.T@e)
-                
-        
-                cost_i = self.cost_vectorised()
-                c_k.append(float(cost_i))
+            #update params acc to learning rate 
+            #old param value minus (learning rate x derivative) 
+            self.th = self.th - self.lr*(1/self.m)*(self.x0.T@e)
             
             
-            else:
-                print(f"num of cols {np.shape(self.x0.T)[1]} not equal to num of rows {np.shape(e)[0]}")
+            #else:
+                #print(f"num of cols {np.shape(self.x0.T)[1]} not equal to num of rows {np.shape(e)[0]}")
     
-        plt.plot(c_k)
     
-        return c_k[-1]
+    
+    
+            cost_i = self.cost_vectorised(self.th)
+            cost_list1.append(cost_i)
+    
+    
+        plt.plot(cost_list1)
+    
+        return cost_list1
 
 
 # In[ ]:
 
 
-#generate random data with seed for reproducibility
-np.random.seed(43)
-a = np.random.randn(10000,10)
-a
-
-
-# In[ ]:
-
-
-np.random.seed(43)
-b = np.random.randn(10000,1)
-b
-
-
-# In[ ]:
-
-
-a = lr(a,b,0.00001,100000)
+a = lr(a,b,0.001,1000)
 
 
 # In[ ]:
@@ -260,19 +284,31 @@ a = lr(a,b,0.00001,100000)
 # In[ ]:
 
 
-#a.gradient_descent_not_vectorised()
+a.gradient_descent_not_vectorised()
 
 
 # In[ ]:
 
 
-a.cost_vectorised()
+#a.cost_vectorised()
 
 
 # In[ ]:
 
 
 a.gradient_descent_vectorised()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
